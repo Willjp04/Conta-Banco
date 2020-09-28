fdbe29d438ae288cead57bd4daffb19feb48d4f0
Willian Sostenes <sostenes655@gmail.com>
2020-09-28 19:20:59 -0300

    Inicio inserção Códigos

---
+++ Conta de Banco/src/conta/de/banco/ContaBanco.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conta.de.banco;
+
+/**
+ *
+ * @author PORTOMASTER
+ */
+public class ContaBanco {
+    public int numConta;
+    protected Character tipoConta;
+    private Character dono;
+    private Real saldo;
+    private String status;
+    
+    
+}
